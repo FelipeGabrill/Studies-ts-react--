6284a2a7a1f6fea71e48e97c94dcfd5fa8fb0ff4
FelipeGabrill <felipegabrielodss@gmail.com>
2024-08-26 19:30:49 -0300

    Criando o cronômetro

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Formulario from '../componets/Formulario';
 import Lista from '../componets/Lista';
 import style from './App.module.scss';
+import { Cronometro } from '../componets/Cronometro';
 
 function App() {
   return (
     <div className={style.AppStyle}>
       <Formulario />
       <Lista />
+      <Cronometro />
     </div>
   );
 }

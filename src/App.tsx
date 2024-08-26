e8d9aa4722e18198973d67ed467f08013f83ab34
FelipeGabrill <felipegabrielodss@gmail.com>
2024-08-26 15:01:00 -0300

    Criando um componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Botao from './componets/Botao';
 
 function App() {
   return (
     <div className="App">
-      Hello world!
+      <Botao />
     </div>
   );
 }

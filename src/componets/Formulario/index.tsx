8197c29ee941bc271d045181157a0f26064b9bb5
FelipeGabrill <felipegabrielodss@gmail.com>
2024-08-26 23:11:27 -0300

    Adicionando uma tarefa

--- src/componets/Formulario/index.tsx
+++ src/componets/Formulario/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import Botao from "../Botao";
 import style from './Formulario.module.scss';
+import { ITarefa } from "../../types/tarefa";
 
-class Formulario extends React.Component {
+class Formulario extends React.Component<{
+    setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>
+}>{
     state = {
         tarefa: "",
         tempo: "00:00"
@@ -10,6 +13,7 @@ class Formulario extends React.Component {
 
     adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
         evento.preventDefault();
+        this.props.setTarefas(tarefasAntigas => [...tarefasAntigas, {...this.state}]);
         console.log('state: ' + this.state);
     }
 
@@ -46,7 +50,7 @@ class Formulario extends React.Component {
                         required
                     /> 
                 </div>
-                <Botao>
+                <Botao type="submit">
                     Adicionar
                 </Botao>
             </form>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Formulario from '../componets/Formulario';
 import Lista from '../componets/Lista';
 import style from './App.module.scss';
 import { Cronometro } from '../componets/Cronometro';
+import { ITarefa } from '../types/tarefa';
 
 function App() {
+  const [tarefas, setTarefas] = useState<ITarefa[]>([]);
+
   return (
     <div className={style.AppStyle}>
-      <Formulario />
-      <Lista />
+      <Formulario setTarefas={setTarefas}/>
+      <Lista tarefas={tarefas}/>
       <Cronometro />
     </div>
   );
